4c8af4650de1060c755dcaf11e98c2ca189bf242
poojagupta-github <mailpoojagupta4@gmail.com>
2021-04-05 23:12:55 +0530

    music

---
+++ music/app.js
@@ -0,0 +1,30 @@
+let mysong = new Audio();
+mysong.src = 'memories.mp3';
+const playsong = () =>{
+    mysong.play();
+}
+
+const pausesong = () =>{
+    mysong.pause();
+}
+
+
+let mysong1 = new Audio();
+mysong1.src = 'up.mp3';
+const playsong1 = () =>{
+    mysong1.play();
+}
+
+const pausesong1 = () =>{
+    mysong1.pause();
+}
+
+let mysong2 = new Audio();
+mysong2.src = 'lol.mp3';
+const playsong2 = () =>{
+    mysong2.play();
+}
+
+const pausesong2 = () =>{
+    mysong2.pause();
+}
